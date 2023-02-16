b340c00c11ee6d67eec5cfea88ebc1a8c2149a7f
CJmean4ik <gromovstas64@gmail.com>
2023-02-16 11:12:42 +0200

    Добавил в к класс WorkArrays такие поля, которые являются делегатми: Writer GetArray Sorter Searcher Проинициализировал их в конструкторе

--- WorkArrays.cs
+++ WorkArrays.cs
@@ -9,8 +9,19 @@ namespace CodeWars
 {
     public class WorkArrays
     {
-       
-      
+        private readonly Action<int[]> Writer;
+        private readonly Func<int[]> GetArray;
+        private readonly Func<int[], int[]> Sorter;
+        private readonly Action<int[]> Searcher;
+
+        //В будущем можно использовать паттерн строитель для инициализации этих полей 
+        public WorkArrays(Action<int[]> writer, Func<int[]> getArray, Func<int[], int[]> sorter, Action<int[]> searcher)
+        {
+            Writer = writer;
+            GetArray = getArray;
+            Sorter = sorter;
+            Searcher = searcher;
+        }
     }
 
 }
